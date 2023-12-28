b002fdd74fac449053a65e787f308e38e9cd15a2
jeangondorek <jeangondorek73@gmail.com>
2023-12-28 14:52:42 +0000

    update

--- src/Routes/GetAllUsers.js
+++ src/Routes/GetAllUsers.js
@@ -6,49 +6,76 @@ class GetAllUsers extends Component {
     state = {
         person: [],
     }
+
     async componentDidMount() {
+        await this.fetchUsers();
+    }
+
+    async componentDidUpdate(prevProps, prevState) {
+        // Verifica se houve uma mudança no estado antes de buscar os usuários novamente
+        if (prevState.person !== this.state.person) {
+            await this.fetchUsers();
+        }
+    }
+
+    async fetchUsers() {
         const response = await api.get('/person');
         this.setState({ person: response.data });
     }
 
-    render() {
-
-    const { person } = this.state;
-
-    return (
-        <>
-        <div className='ml-8 mt-8'>
-            <h1 className='text-4xl'>Lista de usuários</h1>
-            {person.map(user => (
-                <>
-                <table>
-                    <tr>
-                        <td className="p-2 w-20 shadow rounded">
-                            ID
-                        </td>
-                        <td className="p-2 w-80 shadow rounded">
-                            {user._id}
-                        </td>
-                        <td className="p-2 w-20 shadow rounded">
-                            Nome
-                        </td>
-                        <td className="p-2 w-80 shadow rounded">
-                            {user.name}
-                        </td>
-                        <td className="p-2 w-20 shadow rounded">
-                            Salário
-                        </td>
-                        <td className="p-2 w-20 shadow rounded">
-                            {user.salary}
-                        </td>
-                    </tr>
-                </table>
-                </>
-            ))}
-        </div>
-        </>
-        );
+    handleDeleteUser = async (id) => {
+        try {
+            await api.delete(`/person/${id}`);
+            // Atualiza o estado, desencadeando uma nova busca pelos usuários
+            this.setState(prevState => ({ person: prevState.person.filter(user => user._id !== id) }));
+        } catch (error) {
+            console.error("Erro ao excluir usuário:", error);
+            // Trate o erro de exclusão conforme necessário
+        }
     };
-};
+
+    render() {
+        const { person } = this.state;
+
+        return (
+            <>
+                <div className='ml-8 mt-8'>
+                    <h1 className='text-4xl'>Lista de usuários</h1>
+                    {person.map(user => (
+                        <table key={user._id}>
+                            <tbody>
+                                <tr>
+                                    <td className="p-2 w-20 shadow rounded">
+                                        ID
+                                    </td>
+                                    <td className="p-2 w-80 shadow rounded">
+                                        {user._id}
+                                    </td>
+                                    <td className="p-2 w-20 shadow rounded">
+                                        Nome
+                                    </td>
+                                    <td className="p-2 w-80 shadow rounded">
+                                        {user.name}
+                                    </td>
+                                    <td className="p-2 w-20 shadow rounded">
+                                        Salário
+                                    </td>
+                                    <td className="p-2 w-20 shadow rounded">
+                                        {user.salary}
+                                    </td>
+                                    <td>
+                                        <button onClick={() => this.handleDeleteUser(user._id)}>
+                                            Excluir
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    ))}
+                </div>
+            </>
+        );
+    }
+}
 
 export default GetAllUsers;
